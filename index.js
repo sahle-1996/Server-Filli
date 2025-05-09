2c9dbf369e7e0442fad7de0d664535e3693bc02c
Sahle Goitom <sahlesiumgoitom@gmail.com>
2025-05-09 12:09:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL, "http://localhost:5173"],
+    origin: ["https://client-filli-cgnx.vercel.app", "http://localhost:5173"],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
